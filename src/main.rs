4385170998941115a9ca458908ffdf6b839306b5
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2023-01-01 15:26:08 +0900

    refactor impl view func

--- src/main.rs
+++ src/main.rs
@@ -72,6 +72,30 @@ fn input(
     Ok(())
 }
 
+fn view<T: std::io::Write>(
+    output: &mut T,
+    field: &[[Masu; 8]; 8],
+    cursor: &(usize, usize),
+) -> Result<()> {
+    execute!(output, MoveTo(0, 0))?;
+    for y in 0..8 {
+        for x in 0..8 {
+            if x == cursor.0 && y == cursor.1 {
+                execute!(output, SetBackgroundColor(Color::Reset),)?;
+            } else {
+                execute!(output, SetBackgroundColor(Color::DarkGreen),)?;
+            }
+            match field[y][x] {
+                Masu::Empty => execute!(output, Print("  "))?,
+                Masu::Black => execute!(output, Print('⚫'))?,
+                Masu::White => execute!(output, Print('⚪'))?,
+            }
+        }
+        execute!(output, Print("\n"))?;
+    }
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let mut field = [[Masu::Empty; 8]; 8];
     let mut cursor = (0, 0);
@@ -79,22 +103,7 @@ fn main() -> Result<()> {
     enable_raw_mode()?;
     execute!(std::io::stdout(), Hide, EnterAlternateScreen,)?;
     while !end {
-        execute!(std::io::stdout(), MoveTo(0, 0))?;
-        for y in 0..8 {
-            for x in 0..8 {
-                if x == cursor.0 && y == cursor.1 {
-                    execute!(std::io::stdout(), SetBackgroundColor(Color::Reset),)?;
-                } else {
-                    execute!(std::io::stdout(), SetBackgroundColor(Color::DarkGreen),)?;
-                }
-                match field[y][x] {
-                    Masu::Empty => execute!(std::io::stdout(), Print("  "))?,
-                    Masu::Black => execute!(std::io::stdout(), Print('⚫'))?,
-                    Masu::White => execute!(std::io::stdout(), Print('⚪'))?,
-                }
-            }
-            execute!(std::io::stdout(), Print("\n"))?;
-        }
+        view(&mut std::io::stdout(), &field, &cursor)?;
         input(read()?, &mut field, &mut cursor, &mut end)?;
     }
 
